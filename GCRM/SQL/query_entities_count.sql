921174cfff9292236566ee63ced6052943b5b486
Leonardo Merino <wissenschaftecht@gmail.com>
2025-03-16 23:41:49 -0600

    bump version, and new query to get the count per entities

---
+++ GCRM/SQL/query_entities_count.sql
@@ -0,0 +1,17 @@
+﻿DROP TABLE IF EXISTS temp_entities_count;
+
+CREATE TEMPORARY TABLE temp_entities_count(
+	entidad character varying,
+	cantidad integer
+);
+
+INSERT INTO temp_entities_count(
+	entidad, 
+	cantidad
+)VALUES (
+	'ciudadanos',
+	(SELECT COUNT(*) FROM citizens)
+);
+
+
+SELECT * FROM temp_entities_count;
