e3c3a9741e1435c42b717bb6ac8ddbf16890c63a
merinogale@gmail.com <merinogale@gmail.com>
2025-04-04 22:00:38 -0600

    bump

--- publish.cmd
+++ publish.cmd
@@ -1,5 +1,5 @@
 :: compile
-set version=0.1.1.2-alpha
+set version=0.1.1.3-alpha
 
 set "output_path_x64=.\GCRM\bin\Release\gcrm_%version%_x64"
 set "output_path_x86=.\GCRM\bin\Release\gcrm_%version%_x86"
